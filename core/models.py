7a02ab8da2c00b25ca0317742b92b2c5ad60c30c
Folafolu <ofolafolu@gmail.com>
2024-12-23 10:41:19 +0100

    Location

--- core/models.py
+++ core/models.py
@@ -10,19 +10,19 @@ class Location(models.Model):
         return self.name
 
 
-    class Report(models.Model):
-        STATUS_CHOICES = [
-            ('lost', 'Lost'),
-            ('found', 'Found'),
-        ]
-        title = models.CharField(max_length=255)
-        description = models.TextField()
-        image = models.ImageField(upload_to='upload/', null=True)
-        location = models.ForeignKey(Location, on_delete=models.CASCADE)
-        date_reported = models.DateField(auto_now_add=True)
-        phone_number = models.CharField(max_length=11)
-        status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='lost')
-        reporter = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
+class Report(models.Model):
+    STATUS_CHOICES = [
+        ('lost', 'Lost'),
+        ('found', 'Found'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='upload/', null=True)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    date_reported = models.DateField(auto_now_add=True)
+    phone_number = models.CharField(max_length=11)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='lost')
+    reporter = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
